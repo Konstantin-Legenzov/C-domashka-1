8e4a2043719633e97fba7e8de9de753d0420119b
Konstantin <legenzov-k@ya.ru>
2022-10-23 17:56:50 +0300

    OK_001

---
+++ zadacha_001/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача показывающее большее и меньшее число
+
+int a, b;
+
+
+string userEnter, userEnter2;
+
+Console.WriteLine("Введите ПЕРВОЕ число: ");
+userEnter = Console.ReadLine()!;
+
+a = int.Parse(userEnter);
+
+Console.WriteLine("Введите второе число: ");
+userEnter2 = Console.ReadLine()!;
+
+b = int.Parse(userEnter2);
+
+if(a<b)
+{
+   Console.WriteLine($"Минимальное число = {a}");
+   Console.WriteLine($"Максимальное число = {b}");
+}
+else
+{
+    Console.WriteLine($"Минимальное число = {b}");
+    Console.WriteLine($"Максимальное число = {a}"); 
+}
