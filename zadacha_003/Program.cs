04ac81b308d740bd120d7b3b83c9b1aa0ad2db12
Konstantin <legenzov-k@ya.ru>
2022-10-23 19:26:18 +0300

    004_ok

--- zadacha_003/Program.cs
+++ zadacha_003/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача написать программу которая принимает на вход число и выдаёт является ли оно чтным или нет
+
+int a;
+string UserEnter;
+
+Console.Write("Введите число: ");
+UserEnter = Console.ReadLine()!;
+a = int.Parse(UserEnter);
+
+if(a%2 != 0)
+    {
+    Console.Write("Нечётное число");
+    }
+
+else
+    {
+    Console.Write("Чётное число");
+    }
+
