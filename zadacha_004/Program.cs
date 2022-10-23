0ef3a9618febd5936cd36b95e6d076bcc3b90d6c
Konstantin <legenzov-k@ya.ru>
2022-10-23 21:24:31 +0300

    NO_004

--- zadacha_004/Program.cs
+++ zadacha_004/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Написать программу, которая на вход принимает число (N), 
+на выходе показывает все чётные числа от 1 до N.*/
+
+
+using System;
+ 
+class Program {
+ public static int Main() {
+  for(int i=0; i<=20; i++) {
+   if(i%2 == 0) {
+    Console.Write("{0} ", i);
+   }
+  }
+  Console.ReadKey();
+  return 0;
+ }
+}
